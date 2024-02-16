5791b2b7e514352ef8c1e98600f3dc8750cbd94d
Hoccyy <94509399+Hoccyy@users.noreply.github.com>
2024-02-15 20:38:04 -0500

    Fixes

--- fit-pick/src/app/closet/page.tsx
+++ fit-pick/src/app/closet/page.tsx
@@ -1,16 +1,16 @@
 'use client'
 import React, { useEffect, useState } from 'react';
 import SmallOutfitCard from '@/app/components/SmallOutfitCard';
-import { Key } from 'react';
 import { FetchCloset } from '../components/FetchCloset';
 import styles from "../page.module.css";
 
 const App = () => {
-  const [outfitList, setOutfitList] = useState<any[]>([]);
+  const [outfitList, setOutfitList] = useState<string[]>([]);
+  const [loaded, setLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     FetchCloset()
-      .then((list: any) => {
+      .then((list: string[]) => {
         setOutfitList(list);
       })
       .catch((error: any) => {
@@ -18,18 +18,31 @@ const App = () => {
       });
   }, []);
 
-  // Render your components based on outfitList
-  const outfitElements = outfitList.map((imageSource: string, index: Key | null | undefined) => (
-    <SmallOutfitCard key={index} ImageSource={imageSource} />
-  ));
+  useEffect(() => {
+    const images = document.querySelectorAll('.outfit-image');
+    const imageLoadPromises = Array.from(images).map(image =>
+      new Promise(resolve => {
+        if (image.complete) {
+          resolve;
+        } else {
+          image.addEventListener('load', resolve);
+        }
+      })
+    );
+    Promise.all(imageLoadPromises).then(() => {
+      setLoaded(true);
+    });
+  }, [outfitList]);
 
   return (
     <main className={styles.main}>
-            <div>
+      <div>
         <h1 className={styles.AppName}>Closet</h1>
-      <div className={styles.MainClosetFrame}>
-        {outfitElements}
-      </div>
+        <div className={`${styles.MainClosetFrame} ${loaded ? styles.fadeIn : ''}`}>
+          {outfitList.map((imageSource: string, index: number) => (
+            <SmallOutfitCard key={index} ImageSource={imageSource} />
+          ))}
+        </div>
       </div>
     </main>
   );
--- fit-pick/src/app/context/AuthContext.js
+++ fit-pick/src/app/context/AuthContext.js
@@ -1,17 +1,29 @@
 'use client'
 import { useState, useContext, createContext, useEffect } from 'react';
-import { signInWithRedirect, signOut, onAuthStateChanged, GoogleAuthProvider } from 'firebase/auth';
+import { signInWithRedirect, signInWithPopup, signOut, onAuthStateChanged, GoogleAuthProvider } from 'firebase/auth';
 import { auth } from '../../../firebase';
 
 const AuthContext = createContext ()
 
+
+
 export const AuthContextProvider = ({children}) => {
     const [user, setUser] = useState(null)
 
-    const googleSignIn = () => {
-        const provider = new GoogleAuthProvider();
-        signInWithRedirect(auth, provider);
+    const userAgent = navigator.userAgent;
+    let googleSignIn;
+    if (userAgent.toLowerCase().includes('safari')) {
+        googleSignIn = () => {
+            const provider = new GoogleAuthProvider();
+            signInWithPopup(auth, provider);
+        };
+    } else {
+        googleSignIn = () => {
+            const provider = new GoogleAuthProvider();
+            signInWithRedirect(auth, provider);
+        };
     }
+
 
     const logOut = () => {
         signOut(auth);
