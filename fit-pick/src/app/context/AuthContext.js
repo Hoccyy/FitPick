c456d1d16954d13bd95ce63716fbfe789ca87442
Hoccyy <94509399+Hoccyy@users.noreply.github.com>
2024-02-19 17:03:02 -0500

    Auth Changes

--- fit-pick/src/app/context/AuthContext.js
+++ fit-pick/src/app/context/AuthContext.js
@@ -3,41 +3,68 @@ import { useState, useContext, createContext, useEffect } from 'react';
 import { signInWithRedirect, signInWithPopup, signOut, onAuthStateChanged, GoogleAuthProvider } from 'firebase/auth';
 import { auth } from '../../../firebase';
 
+
 const AuthContext = createContext ()
 
 
-
 export const AuthContextProvider = ({children}) => {
-    const [user, setUser] = useState(null)
+    try {
+        const [user, setUser] = useState(null)
+        const userAgent = (navigator.userAgent);
 
-    const userAgent = navigator.userAgent;
-    let googleSignIn;
-    if (userAgent.toLowerCase().includes('safari')) {
-        googleSignIn = () => {
-            const provider = new GoogleAuthProvider();
-            signInWithPopup(auth, provider);
-        };
-    } else {
+        let googleSignIn;
+        
+        if (userAgent.toLowerCase().includes('safari')) {
+            googleSignIn = () => {
+                const provider = new GoogleAuthProvider();
+                signInWithPopup(auth, provider);
+            };
+        } else {
+            googleSignIn = () => {
+                const provider = new GoogleAuthProvider();
+                signInWithRedirect(auth, provider);
+            };
+        }
+
+
+        const logOut = () => {
+            signOut(auth);
+        }
+
+        useEffect (() => {
+            const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+                setUser(currentUser);
+            });
+            return () => unsubscribe();
+        }, [user])
+        return (
+            <AuthContext.Provider value={{user, googleSignIn, logOut}}>{children}</AuthContext.Provider>
+        )
+    }
+
+    catch {
+        let googleSignIn;
+        
         googleSignIn = () => {
             const provider = new GoogleAuthProvider();
             signInWithRedirect(auth, provider);
         };
+
+
+        const logOut = () => {
+            signOut(auth);
+        }
+
+        useEffect (() => {
+            const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+                setUser(currentUser);
+            });
+            return () => unsubscribe();
+        }, [user])
+        return (
+            <AuthContext.Provider value={{user, googleSignIn, logOut}}>{children}</AuthContext.Provider>
+        )
     }
-
-
-    const logOut = () => {
-        signOut(auth);
-    }
-
-    useEffect (() => {
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser);
-        });
-        return () => unsubscribe();
-    }, [user])
-    return (
-        <AuthContext.Provider value={{user, googleSignIn, logOut}}>{children}</AuthContext.Provider>
-    )
 }
 
 export const UserAuth = () => {
